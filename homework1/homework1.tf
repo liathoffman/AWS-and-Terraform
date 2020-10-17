3c6ca28153e3540e7b9b4f48dfc3961e0f027391
liathoffman <liatshoffman@gmail.com>
2020-10-17 19:32:45 +0300

    homework1

---
+++ homework1/homework1.tf
@@ -0,0 +1,159 @@
+
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {}
+variable "region" {
+  default = "us-east-1"
+}
+
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
+}
+
+##################################################################################
+# DATA
+##################################################################################
+
+data "aws_ami" "aws-linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+#This uses the default VPC.  It WILL NOT delete it on destroy.
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "nginx_demo"
+  description = "Allow ports for nginx demo"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx1" {
+  ami                    = data.aws_ami.aws-linux.id
+  instance_type          = "t2.medium"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  user_data = "${file("install_nginx.sh")}"
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.private_key_path)
+
+  }
+
+  tags = {
+    Name  = "Nginx server 1"
+    Env   = "Development"
+    Owner = "Liat"
+  }
+
+  ebs_block_device {
+    device_name = "xvdf"
+    volume_type = "gp2"
+    volume_size = 10
+    encrypted   = true
+  }
+
+}
+
+
+resource "aws_instance" "nginx2" {
+  ami                    = data.aws_ami.aws-linux.id
+  instance_type          = "t2.medium"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  user_data = "${file("install_nginx.sh")}"
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.private_key_path)
+
+  }
+
+  tags = {
+    Name  = "Nginx server 2"
+    Env   = "Development"
+    Owner = "Liat"
+  }
+
+  ebs_block_device {
+    device_name = "xvdf"
+    volume_type = "gp2"
+    volume_size = 10
+    encrypted   = true
+  }
+
+}
+
+
+
+##################################################################################
+# OUTPUT
+##################################################################################
+
+output "aws_instance_public_dns_nginx1" {
+  value = aws_instance.nginx1.public_dns
+}
+
+output "aws_instance_public_dns_nginx2" {
+  value = aws_instance.nginx2.public_dns
+}
+
