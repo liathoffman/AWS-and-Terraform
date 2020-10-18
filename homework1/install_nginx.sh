8bc85ac0903ff3a730184c30603b4fdb7ab72fdc
liathoffman <liatshoffman@gmail.com>
2020-10-18 10:50:32 +0300

    OpsSchool Rules

--- homework1/install_nginx.sh
+++ homework1/install_nginx.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 sudo yum install nginx -y
 sudo service nginx start
+# removing old index.html file from ngninx
+sudo rm /usr/share/nginx/html/index.html
+sudo touch /usr/share/nginx/html/index.html
+# Creating index.html file
+sudo chmod 777 /usr/share/nginx/html -R
+sudo cat > /usr/share/nginx/html/index.html << EOF
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <title>This is a Title</title>
+        <meta charset="utf-8" />
+</head>
+<body class="container">
+        <h1>OpsSchool rules!<h1>
+
+
+</body>
+</html>
+EOF
