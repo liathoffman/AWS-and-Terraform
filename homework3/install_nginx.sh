07805229aca743165b108404325ad8992d869c75
liathoffman <liatshoffman@gmail.com>
2020-11-07 15:12:16 +0200

    added hostname to welcome page on 2 webservers

--- homework3/install_nginx.sh
+++ homework3/install_nginx.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+HOSTNAME = curl http://169.254.169.254/latest/meta-data/hostname
 sudo apt-get update
 sudo apt-get install -y nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
-echo "This is server number $RANDOM" | sudo tee /var/www/html/index.html
+echo "This is server with hostname $HOSTNAME" | sudo tee /var/www/html/index.html
