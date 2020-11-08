88bd7179066445613d17478e879db4d0abb0a89d
liathoffman <liatshoffman@gmail.com>
2020-11-08 02:35:40 +0200

    trying to debug cron job, which is not running

--- homework3/install_nginx.sh
+++ homework3/install_nginx.sh
@@ -6,11 +6,14 @@ sudo systemctl start nginx
 sudo systemctl enable nginx
 echo "This is server with hostname $HOSTNAME" | sudo tee /var/www/html/index.html
 sudo apt  install awscli -y
-sudo /etc/init.d/cron start
-sudo chmod 777 /etc/cron.d
+sudo chmod 777 /etc/cron.hourly
 
-sudo cat <<EOF > /etc/cron.d/accessloghourly
+sudo cat <<EOF > /etc/cron.hourly/accessloghourly
 #!/bin/bash
 0 * * * * sudo aws s3 cp /var/log/nginx/access.log s3://liat-nginx-logs-282837837882/$HOSTNAME-access.log"
-chmod +x accessloghourly
 EOF
+
+sudo chmod +x /etc/cron.hourly/accessloghourly
+sudo /etc/init.d/cron start
+
+# need to figure out ho to get this working... and test it
