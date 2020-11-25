6f02224b15f3caa365951cb7ed181cb4923db96c
liathoffman <liatshoffman@gmail.com>
2020-11-25 20:06:54 +0200

    changing to new strategy for creating the cron job - instead of using ansible, now elevating privelages to root, and all probles see to be solved

--- homework3/install_nginx.sh
+++ homework3/install_nginx.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
+sudo -i
 HOSTNAME = curl http://169.254.169.254/latest/meta-data/hostname
-sudo apt-get update
-sudo apt-get install -y nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx
+apt-get update
+apt-get install -y nginx
+systemctl start nginx
+systemctl enable nginx
 echo "This is server with hostname $HOSTNAME" | sudo tee /var/www/html/index.html
-sudo apt  install awscli -y
-sudo apt update
-sudo apt install software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible -y
+apt install awscli -y
 
-sudo cat <<EOF > ~/hosts.yml
-hosts:
-  localhost:
-   vars:
-     ansible_connection: local
-     ansible_python_interpreter: "{{ansible_playbook_python}}"
+cat <<EOF > /etc/cron.d/accessloghourly
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 * * * * root aws s3 cp /var/log/nginx/access.log s3://liat-nginx-logs-282837837882
 EOF
 
-sudo ansible localhost -m ansible.builtin.cron -a "name=access-logs minute=0 job='sudo aws s3 cp /var/log/nginx/access.log s3://liat-nginx-logs-282837837882/{{ansible_eth0.ipv4.address}}-access.log'" --become
+chmod +x /etc/cron.d/accessloghourly
+
+/etc/init.d/cron start
 
